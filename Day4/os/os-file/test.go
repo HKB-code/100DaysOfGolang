d5a7a4ad8ff4cc71290e16a0d9635855441f51f6
HkbCodes <harshilj2002@gmail.com>
2024-10-28 22:31:13 +0530

    add some comments

--- Day4/os/os-file/test.go
+++ Day4/os/os-file/test.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"os"
+
+	// In Go, you can use the reflect package to determine the type of a variable.
+	"reflect"
 )
 
 func main(){
@@ -23,7 +26,19 @@ if(err2!=nil){
 for _,file:=range files{
 	fmt.Println(file.Name())
 }
+// Use reflect.TypeOf from the reflect package to determine the type of a variable.
+//When you use os.ReadDir, it returns a slice of fs.DirEntry, representing each file or directory entry in the specified directory.
+fmt.Println(reflect.TypeOf(files))
+/*
+Notes:
+fs generally refers to filesystem-related functionality. The fs package within the io library provides interfaces and abstractions for dealing with filesystem operations.
+# Key Interfaces in io/fs:
+1.fs.FS: The root interface for accessing a filesystem.
 
+2.fs.File: Represents an open file within a filesystem.
+
+3.fs.DirEntry: Represents a directory entry (like a file or directory) within a filesystem.
+*/
 // 3. Create a New Directory
 dirname1:="new_directory"
 err3:=os.Mkdir(dirname1,0755)
@@ -37,6 +52,7 @@ err3:=os.Mkdir(dirname1,0755)
 
 // 4. Check if a File or Directory Exists
 path:="new_directory"
+//In Go, you can dynamically declare a variable within an if statement using :=, which is only valid within the scope of the if block. This approach makes the code concise and clean.
 if _,err4:=os.Stat(path);os.IsNotExist(err4){
 fmt.Printf("'%s' does not exist\n", path)
 }else {
--- Day4/os/os-system/test.go
+++ Day4/os/os-system/test.go
@@ -14,11 +14,41 @@ func main() {
 
 
 // 7. Execute a Shell Command
-		cmd := exec.Command("echo", "Hello, World!")
+/*For Windows Systems:
+
+cmd: The command-line interpreter for Windows.
+
+/C: Flag to carry out the command specified by the string and then terminate.
+
+echo Hello, World!: The command to run.*/
+/*Using cmd with /C in Windows is essential because echo is a built-in command in the Windows Command Prompt, and cmd is the command-line interpreter for executing it.
+
+Breakdown:
+cmd:
+
+The command-line interpreter for Windows, used to run commands in a command prompt.
+
+/C:
+
+Tells cmd to run the specified command and then terminate.
+
+Without /C, the command prompt would stay open or the command might not execute properly.
+
+echo Hello, World!:
+
+The actual command to be executed.
+
+echo is a built-in command in the Windows Command Prompt that outputs the text following it.
+
+Why These Steps Are Needed:
+Execute Built-in Commands: echo and similar commands are built-in commands of the command interpreter (cmd). They are not standalone executable files.
+
+Ensure Proper Execution and Termination: Using cmd /C ensures that the command runs in the command prompt environment and the environment is closed after execution.*/
+		cmd := exec.Command("cmd","/C", "echo Hello, World!")
     output, err := cmd.Output()
-    if err != nil {
+    if(err!=nil){
         fmt.Println("Error:", err)
         return
     }
-    fmt.Printf("Output: %s\n", output)
+    fmt.Printf("Output: %s\n",string(output))
 }
