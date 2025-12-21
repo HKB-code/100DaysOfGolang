5f5f0b62885bedd36149e3d432be38c6f6b7ffbb
HKB-code <harshilj2002@gmail.com>
2025-12-21 12:22:54 +0530

    add some information

--- Day_1/test.go
+++ Day_1/test.go
@@ -4,4 +4,13 @@ import "fmt"
 
 func main(){
 	fmt.Println("Hello World")
+	/*
+	The example shows variables of several types, and also that variable declarations may be "factored" into blocks, as with import statements.
+	*/
+	var(
+		a bool = false
+		b string = "Hi"
+		c int = 1
+	)
+	fmt.Println(a,b,c)
 }
