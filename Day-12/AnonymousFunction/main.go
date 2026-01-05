da2592a75d3eb2badd14d3fb33f9345ecb4ee5f6
HKB-code <harshilj2002@gmail.com>
2026-01-05 14:11:52 +0530

    add a example

--- Day-12/AnonymousFunction/main.go
+++ Day-12/AnonymousFunction/main.go
@@ -2,7 +2,28 @@ package main
 
 import "fmt"
 
+type Person struct {
+	Name  string
+	Age   int
+	Email string
+}
+
+func (p *Person) Greet(retiermentYear func(int) int) string {
+	age := retiermentYear(p.Age)
+	return fmt.Sprintf("Hello %v, you have %v years left for Retierment",p.Name,age)
+}
+
 func main() {
+
+p:=Person{
+		Name: "Jhon",
+		Age: 42,
+		Email:"JhonDev@gmail.com",
+	}
+	fmt.Println(p.Greet(func(age int)int{
+		return 65 - age
+	}))
+
 	numbers := []int{1, 2, 3, 4}
 
 	// Anonymous Function : that anonymous function is a value, not a type.
