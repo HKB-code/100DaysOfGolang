f83fd080309dcf03fa2bec5773bf3f44f77dae89
HKB-code <harshilj2002@gmail.com>
2026-09-24 18:14:31 +0530

    bug fix in getMultipleLines func

--- Day-9-Part2/main.go
+++ Day-9-Part2/main.go
@@ -9,102 +9,96 @@ import (
 	note "example.com/main/note"
 	todo "example.com/main/todo"
 )
-type saver interface{
+
+type saver interface {
 	Save() error
 }
-type outputtable interface{
+type outputtable interface {
 	saver
 	Display()
-} 
-
-
-func SaverData(data saver)error{
-err:= data.Save()
-if  err!= nil {
-	fmt.Println("Saving the data failed")
-	return err
-}
-fmt.Println("Saving the data succeeded")
-return nil
 }
 
-func outputData(data outputtable)error{
+func SaverData(data saver) error {
+	err := data.Save()
+	if err != nil {
+		fmt.Println("Saving the data failed")
+		return err
+	}
+	fmt.Println("Saving the data succeeded")
+	return nil
+}
+
+func outputData(data outputtable) error {
 	data.Display()
 	return SaverData(data)
 
 }
 
-func process (data outputtable){
-	if err:= outputData(data);err!=nil{
+func process(data outputtable) {
+	if err := outputData(data); err != nil {
 		fmt.Printf("Operation failed %v\n", err)
 		os.Exit(1)
 	}
 }
 
-func handleErr(err error){
-	if err!=nil{
+func handleErr(err error) {
+	if err != nil {
 		fmt.Printf("Error: %v\n", err)
 		os.Exit(1)
 	}
 }
 
-
 func getInputs(text string) string {
 	fmt.Println(text)
-	scanner:=bufio.NewScanner(os.Stdin)
-	if scanner.Scan(){
-		return  scanner.Text()
+	scanner := bufio.NewScanner(os.Stdin)
+	if scanner.Scan() {
+		return scanner.Text()
 	}
-	if err:=scanner.Err();err!=nil{
-		fmt.Print("Error",err)
+	if err := scanner.Err(); err != nil {
+		fmt.Print("Error", err)
 	}
 	return ""
 }
 
-func getMultipleLinesInput(prompt string)string{
+func getMultipleLinesInput(prompt string) string {
 	fmt.Println(prompt)
 	var lines []string
-	scanner:=bufio.NewScanner(os.Stdin)
+	scanner := bufio.NewScanner(os.Stdin)
 
-    for scanner.Scan(){
+	for scanner.Scan() {
 		line := strings.TrimSpace(scanner.Text())
-		if line==""{
+		if line == "" {
 			break
 		}
-		lines = append(lines,scanner.Text() )
+		lines = append(lines, scanner.Text())
 
-		    if err := scanner.Err(); err != nil {
-        fmt.Println("Input error:", err)
-        return ""
-    }
-	if len(lines)==0{
-		return  ""
 	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Input error:", err)
+		return ""
 	}
-	return  strings.Join(lines,"\n")
+	return strings.Join(lines, "\n")
 }
 
+func getNoteData() (string, string) {
+	title := getInputs("Note title: ")
 
-func getNoteData()(string,string){
-	title :=getInputs("Note title: ")
-
-
-
-	content:=getMultipleLinesInput("Note content:")
-	return  title,content
+	content := getMultipleLinesInput("Note content:")
+	return title, content
 }
 
-func main(){
-	title,content:=getNoteData()
-	todoText:= getMultipleLinesInput("Enter Todo:")
+func main() {
+	title, content := getNoteData()
+	todoText := getMultipleLinesInput("Enter Todo:")
 	// todo define
-	todo,err:= todo.New(todoText)
+	todo, err := todo.New(todoText)
 	handleErr(err)
-process(todo)
+	process(todo)
 
 	// note define
-	userNote,err:= note.New(title,content)
+	userNote, err := note.New(title, content)
 	handleErr(err)
 
-process(userNote)
+	process(userNote)
 }
